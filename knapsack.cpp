68c3a20cb6a54343bebdd5244b6b7319673d4891
unknown <maxper@kth.se>
2021-01-30 22:48:38 +0100

    Wrote a basic greedy algorithm for the knapsack problem to work with the Kattis implementation.

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
 
+using namespace std;
 
-class Item{    
-    int value, weight;
-    std::string name;
+class Item{
+    // The value of the item, weight of the item and the original index of the item.
+    int value, weight, index;
 
     public:
-    Item( std::string n, int w, int v){ name = n; value = v; weight = w; }
-    const int getValue() const{ return value; }
-    const int getWeight() const{ return weight; }
-    const std::string getName() const{ return name; }
+    // Constructor.
+    Item(int value, int weight, int index){ this->value = value; this->weight = weight; this->index = index; }
+
+    // Getters.
+    const int getValue() const { return value; }
+    const int getWeight() const { return weight; }    
+    const int getIndex() const { return index; }     
+
+    // Operators.
+    bool operator<(const Item& i) const { return value < i.getValue(); }
 };
 
-class Knapsack{
-    int capacity;
-    int weight = 0;
-    std::vector<Item> items;
-
-    public:
-    Knapsack(const int c){ capacity = c; }
+int main(void){
+    // Current knapsack, current list of all items, matrix of items picked per test case.
+    vector<Item> K, I;
+    vector<vector<Item>> R;
+    // Capacity, Number of Objects, Value, Weight, Total Acquired Weight.
+    int c, n, v, w;
+    int t_w = 0;
     
-    void add(Item i){ 
-        if(weight+i.getWeight() <= capacity){
-            items.push_back(i); 
-            weight += i.getWeight();
-            std::cout << "Added " << i.getName() << " to the knapsack." << std::endl;
-        } else
-            std::cout << i.getName() << " is too heavy to fit in the knapsack." << std::endl;        
+    // Reading from Kattis.
+    long long a, b;    
+    while (cin >> a >> b) {
+        // Read meta.
+        cin >> c;
+        cin >> n;
+        // Read items.
+        for(int i=0; i < n; i++){
+            cin >> v;
+            cin >> w;
+            I.push_back(Item(v,w,i));
+        }
+        
+        // Sort with highest value first.
+        sort(I.rbegin(),I.rend());
+
+        // Greedily pick the best items.
+        for(int i=0; i < n; i++){
+            if(t_w + I[i].getWeight() == c){ K.push_back(I[i]); t_w += I[i].getWeight(); break; } // Break if we're full.
+            if(t_w + I[i].getWeight() < c){ K.push_back(I[i]); t_w += I[i].getWeight(); } // Add if we're not.
+        }
+
+        // Append the current knapsack to the knapsack matrix.
+        R.push_back(K);
     }
 
-    const std::vector<Item>& getItems() const { return items; }
-    const int getCapacity() const { return capacity; }
-};
+    // Print number of picked objects.
+    for(int i=0; i < R.size(); i++)
+        cout << R[i].size();
+    
+    // Print the picked objects.
+    for(int i=0; i < R.size(); i++){
+        for(int j=0; j < R[i].size(); j++)
+            cout << R[i][j].getIndex();
+    }
 
-int main(){
-    Item gold_bar("Gold Bar", 100, 2000);
-    Item dates("Dates", 20, 5);
-    Knapsack k(100);
-    k.add(gold_bar);
-    k.add(dates);
+    // Terminate the program.
     return 0;
 }
